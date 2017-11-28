9c62633a188e9f07d833dda6722136d87460f581
Thiago <thia.mdossantos@gmail.com>
2017-11-28 12:23:53 -0200

    :sparkles: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const cssnext = require('postcss-cssnext')
+const cssnano = require('cssnano')
+const easyImport = require('postcss-easy-import')
+const notifier = require('node-notifier')
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'public/static'),
+    publicPath: '/static/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env'],
+              plugins: ['transform-object-rest-spread']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                localIdentName: '[local]--[hash:base64]'
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => {
+                  return [
+                    easyImport(),
+                    cssnext({
+                      warnForDuplicates: false
+                    }),
+                    cssnano({
+                      discardUnused: {
+                        fontFace: false,
+                        keyframes: false
+                      },
+                      zindex: false,
+                      reduceIdents: false
+                    })
+                  ]
+                }
+              }
+            }
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('index.css'),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new FriendlyErrorsWebpackPlugin({
+      onErrors(severity, errors) {
+        if (severity !== 'error') {
+          return
+        }
+        const error = errors[0]
+
+        notifier.notify({
+          title: 'Webpack error',
+          message: severity + ': ' + error.name,
+          subtitle: error.file || ''
+        })
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: path.resolve(__dirname, 'public/index.html')
+    })
+  ]
+}
